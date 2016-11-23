21126f037b5161d011ad6f8f64b9a60033f8bc5d
Minhas Kamal <minhaskamal024@gmail.com>
2016-11-23 08:18:41 +0600

    creepy code collection

---
+++ hello_world.java
@@ -0,0 +1,6 @@
+public class not_so_random{
+    public static void main (String[] args) {
+        for(long l=4946144450195624l; l>0; l>>=5)
+            System.out.print((char) (((l & 31 | 64) % 95) + 32));
+    }
+}
