dfe06bb0fd9ad31ec79c78b156d6a4e35492ad3c
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-15 11:17:05 +0600

    It is surprising enough, that the code compiles & prints 1

--- confusion.java
+++ confusion.java
@@ -1,3 +1,6 @@
+// Source- http://stackoverflow.com/questions/7924961/weird-java-behavior-with-casts-to-primitive-types
+// It is surprising enough, that the code compiles & prints 1.
+
 class confusion{
     
 	public static void main (String[] args){
