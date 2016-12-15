1e2efc79e2eb3ed3f4c716af3a32b616e3c8f80c
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-15 11:24:39 +0600

    It is surprising enough, that the code compiles & prints 1.

--- confusion.java
+++ confusion.java
@@ -3,8 +3,8 @@
 
 class confusion{
     
-	public static void main (String[] args){
-    	int i = (byte) + (char) - (int) + (long) - 1;
-	    System.out.println(i);
-	}
+    public static void main (String[] args){
+        int i = (byte) + (char) - (int) + (long) - 1;
+        System.out.println(i);
+    }
 }
