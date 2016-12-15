ef2e4e1449251d8758c273df5e939542579ab8d8
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-15 08:15:40 +0600

    pi signature

--- pi_signature.c
+++ pi_signature.c
@@ -1,1 +1,4 @@
+// Source- http://www.iwriteiam.nl/SigProgC.html#pi
+// Calculates the first 15000 digits of PI.
+
 a[52514],b,c=52514,d,e,f=1e4,g,h;main(){for(;b=c-=14;h=printf("%04d",e+d/f))for(e=d%=f;g=--b*2;d/=g)d=d*b+f*(h?a[b]:f/5),a[b]=d%--g;}
