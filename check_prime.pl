9a33a25c6f3d972f80cb3254581b153649fcb482
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-15 14:13:00 +0600

    Checks if a number is prime using regular expression.

--- check_prime.pl
+++ check_prime.pl
@@ -1,5 +1,5 @@
 # Source- http://neilk.net/blog/2000/06/01/abigails-regex-to-test-for-prime-numbers/
 # Checks if a number is prime using regular expression.
-
+ 
 $a=33;
 print "prime" if(1x$a)!~/^1?$|^(11+?)\1+$/;
