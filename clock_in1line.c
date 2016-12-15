8498556065f86c70d111ceb50213e634cdfc67fb
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-15 14:13:09 +0600

    Prints current time in text-art.

--- clock_in1line.c
+++ clock_in1line.c
@@ -1,4 +1,4 @@
 // Source- http://www.ioccc.org/years.html#2006
 // Prints current time in text-art.
-
+ 
 main(_){_^448&&main(-~_);putchar(--_%64?32|-~7[__TIME__-_/8%8][">'txiZ^(~z?"-48]>>";;;====~$::199"[_*2&8|_/64]/(_&2?1:8)%8&1:10);}
