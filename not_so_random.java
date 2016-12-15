a03487ab77b4b7ac3004d28fa24ce351bce08005
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-15 11:08:40 +0600

    not so random

--- not_so_random.java
+++ not_so_random.java
@@ -1,9 +1,11 @@
+// Source- http://vanillajava.blogspot.com/2011/10/randomly-no-so-random.html
+// Will (randomly!) print- 1 1 1 1 1 1 1 1 1 1.
+
 import java.util.Random;
 
 public class not_so_random{
     
 	public static void main (String[] args){
-	    
 		Random random = new Random(441287210);
 	    for(int i=0;i<10;i++){
 	        System.out.print(random.nextInt(10)+" ");
