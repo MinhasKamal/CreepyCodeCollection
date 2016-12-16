b58081e04363e49447fe8e2ec98d59fc66809e67
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-16 14:51:39 +0600

    Computes day of the week for given date.

---
+++ day_of_week.c
@@ -0,0 +1,12 @@
+// Source- https://en.wikipedia.org/wiki/Determination_of_the_day_of_the_week#Implementation-dependent_methods
+// Computes day of the week for given date.
+
+#include <stdio.h>
+
+main(){
+    printf("%d %d %d", dow(2011, 7, 19), dow(1993, 5, 23), dow(2016, 12, 16)); //output- 2 0 5
+}
+
+int dow(int y, int m, int d) {
+    return ((y-=m<3)+y/4-y/100+y/400+" bed=pen+mad."[m]+d)%7;
+}
