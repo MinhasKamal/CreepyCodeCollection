c3c728c3a77e0178c52e95f052c9588ed0ec3148
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-19 23:51:01 +0600

    Prints prime numbers in the console.

--- signature_prime.c
+++ signature_prime.c
@@ -1,4 +1,4 @@
 // Developer- Minhas Kamal
-// Prints prime numbers in the console.
+// Prints prime numbers in the console. Number of characters- 84.
 
-main(){int $=1,_;while($+=2)for(_=3;1<_&&(_<=sqrt($)?_:printf("%i|",$)<0);_=$%_?_+2:1);}
+main($,_){for($=3;;$+=2)for(_=3;1<_&&(_<=sqrt($)?_:printf("%i|",$)<0);_=$%_?_+2:1);}
