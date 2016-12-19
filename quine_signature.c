9de64dbcdc5928440411c0825135dcb3056e8e27
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-19 23:39:09 +0600

    Shortest C code that prints itself.

--- quine_signature.c
+++ quine_signature.c
@@ -1,4 +1,4 @@
 // Source- http://www.iwriteiam.nl/SigProgS.html#dd
-// Prints itself.
+// Shortest C code that prints itself.
  
-main(a){printf(a="main(a){printf(a=%c%s%c,34,a,34);}", 34, a, 34);}
+main(a){printf(a="main(a){printf(a=%c%s%c,34,a,34);}",34,a,34);}
