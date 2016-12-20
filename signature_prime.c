dc0bece8d2900469d1847da48e3bb400148db28f
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-20 16:39:12 +0600

    Keeps printing prime number (theoretically) forever.

--- signature_prime.c
+++ signature_prime.c
@@ -1,4 +1,4 @@
 // Developer- Minhas Kamal
-// Prints prime numbers in the console. Number of characters- 84.
+// Keeps printing prime number (theoretically) forever. Number of characters- 83.
 
-main($,_){for($=3;;$+=2)for(_=3;1<_&&(_<=sqrt($)?_:printf("%i|",$)<0);_=$%_?_+2:1);}
+main($,_){for($=3;;$+=2)for(_=3;1<_&(_<=sqrt($)?_:printf("%i|",$)<0);_=$%_?_+2:1);}
