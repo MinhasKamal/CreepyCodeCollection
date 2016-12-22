0b0ee67ad87f009691ebc076b74332bb927d631f
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-22 16:16:29 +0600

    Prints current time in text-art.

--- clock_in1line.c
+++ clock_in1line.c
@@ -1,4 +1,5 @@
-// Source- http://www.ioccc.org/years.html#2006
-// Prints current time in text-art.
+// Developer- Stephen Sykes (http://www.stephensykes.com/)
+// Intro- Prints current time in text-art.
+// Details- http://stackoverflow.com/questions/15393441/obfuscated-c-code-contest-2006-please-explain-sykes2-c
  
 main(_){_^448&&main(-~_);putchar(--_%64?32|-~7[__TIME__-_/8%8][">'txiZ^(~z?"-48]>>";;;====~$::199"[_*2&8|_/64]/(_&2?1:8)%8&1:10);}
