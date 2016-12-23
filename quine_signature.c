a9cd13032e30617c554a7b59b5292c92ba8b966e
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-23 10:18:42 +0600

    Shortest C code that prints itself.

--- quine_signature.c
+++ quine_signature.c
@@ -1,4 +1,5 @@
-// Source- http://www.iwriteiam.nl/SigProgS.html#dd
-// Shortest C code that prints itself.
+// Developer- Dario Dariol
+// Intro- Shortest C code that prints itself.
+// Details- http://stackoverflow.com/questions/29837173/how-does-this-program-duplicate-itself
  
 main(a){printf(a="main(a){printf(a=%c%s%c,34,a,34);}",34,a,34);}
