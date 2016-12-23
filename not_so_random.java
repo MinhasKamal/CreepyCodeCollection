ab6c2175c425c27f90a52abc08c1d514d0931e9c
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-23 12:38:52 +0600

    Will (randomly!) print- 1 1 1 1 1 1 1 1 1 1.

--- not_so_random.java
+++ not_so_random.java
@@ -1,14 +1,15 @@
-// Source- http://vanillajava.blogspot.com/2011/10/randomly-no-so-random.html
-// Will (randomly!) print- 1 1 1 1 1 1 1 1 1 1.
- 
+// Intro- Will (randomly!) print- 1 1 1 1 1 1 1 1 1 1.
+// Details- http://stackoverflow.com/questions/13241937/random-class-acting-odd
+
 import java.util.Random;
 
 public class not_so_random{
     
-	public static void main (String[] args){
-		Random random = new Random(441287210);
-	    for(int i=0;i<10;i++){
-	        System.out.print(random.nextInt(10)+" ");
+    public static void main (String[] args){
+        Random random = new Random(441287210);
+        for(int i=0;i<10;i++){
+            System.out.print(random.nextInt(10)+" ");
         }
-	}
+    }
+    
 }
