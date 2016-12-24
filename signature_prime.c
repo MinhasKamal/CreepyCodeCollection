bf482f449bddcc9c473514a67f04dde38f93e165
Minhas Kamal <minhaskamal024@gmail.com>
2016-12-25 00:10:06 +0600

    Keeps printing prime number (theoretically) forever.

--- signature_prime.c
+++ signature_prime.c
@@ -1,6 +1,6 @@
 // Developer- Minhas Kamal (https://minhaskamal.github.io)
-// Intro- Keeps printing prime number (theoretically) forever. Number of characters- 80.
-//      Can be made even shorter (69 bytes)- main($,_){for(;$++;)for(_=2;_&(_<$?_:!printf("%i|",$));_=$%_?_+1:0);}
+// Intro- Keeps printing prime number (theoretically) forever. Number of characters- 76.
+//      Can be made even shorter (65 bytes)- main($,_){for(;$++;)for(_=2;_<$?_:!printf("%i|",$);_=$%_?_+1:0);}
 // Details- https://www.quora.com/What-is-the-shortest-and-most-effective-code-ever-written/answer/Minhas-Kamal?srid=u2HNX
 
-main($,_){for($=3;;$+=2)for(_=3;_&(_<=sqrt($)?_:!printf("%i|",$));_=$%_?_+2:0);}
+main($,_){for($=3;;$+=2)for(_=3;_<=sqrt($)?_:!printf("%i|",$);_=$%_?_+2:0);}
