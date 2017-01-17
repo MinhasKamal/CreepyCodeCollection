d0632ccf2f2d843fe88fa361efab2730846c48fc
Minhas Kamal <minhaskamal024@gmail.com>
2017-01-17 23:56:30 +0600

    Reads a spelt number and writes a corresponding decimal number.

---
+++ BestShortProgram.c
@@ -0,0 +1,9 @@
+// Developer- Seonghoon Kang
+// Intro- Reads a spelt number and writes a corresponding decimal number.
+// Details- http://www.ioccc.org/2012/kang/hint.html
+
+long long n,u,m,b;main(e,r)char **r;{f\
+or(;n++||(e=getchar()|32)>=0;b="ynwtsflrabg"[n%=11]-e?b:b*8+
+n)for(r=b%64-25;e<47&&b;b/=8)for(n=19;n;n["1+DIY/.K430x9\
+G(kC["]-42&255^b||(m+=n>15?n:n>9?m%u*~-u:~(int)r?n+
+!(int)r*16:n*16,b=0))u=1ll<<6177%n--*4;printf("%llx\n",m);}
