7534c1ff646235249b1c80057962ecc749aef464
Minhas Kamal <minhaskamal024@gmail.com>
2017-01-18 00:58:43 +0600

    A quine that runs in c, python, perl & ruby.

---
+++ polyglot_quine.c.py.pl.rb
@@ -0,0 +1,13 @@
+#define Intro "A quine that runs in c, python, perl & ruby."
+#define Source "http://shinh.skr.jp/obf/"
+
+ #include/*
+s='''*/<stdio.h>
+main(){char*_;/*==;sub _:lvalue{$_}<<s;#';<<s#'''
+def printf(a,*b):print a%b,
+s
+#*/
+_=" #include/*%cs='''*/<stdio.h>%cmain(){char*_;/*==;sub _:lvalue{%c_}<<s;#';<<s#'''%cdef printf(a,*b):print a%%b,%cs%c#*/%c_=%c%s%c;printf(_,10,10,36,10,10,10,10,34,_,34,10,10,10,10);%c#/*%cs='''*/%c}//'''#==%c";printf(_,10,10,36,10,10,10,10,34,_,34,10,10,10,10);
+#/*
+s='''*/
+}//'''#==
