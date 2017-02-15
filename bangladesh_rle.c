9e384c1c57a5140f5efbf50b08d3e31dd2700f60
Minhas Kamal <minhaskamal024@gmail.com>
2017-02-16 00:46:47 +0600

    Prints The Map of Bangladesh.

--- bangladesh_rle.c
+++ bangladesh_rle.c
@@ -1,3 +1,7 @@
+// Developer: Victor Adam (https://github.com/Grimy) & Minhas Kamal (https://minhaskamal.github.io/)
+// Intro- Prints The Map of Bangladesh.
+// Inspired- http://stackoverflow.com/questions/3533348/how-does-this-code-generate-the-map-of-india
+
 b;c;char*m="'$R'$$L,%$E/#%C5C4F1G1EK+M'O'O)M-H2D2@6?)%.?('.\
 ?'(.?%)/@#*/H/I.=#-.<#/-<$..6#&&,.+$('%&,/$=+L+N%$&L%S%T$&";
 main(i){while(--i<34?i=m[c++]:putchar(b++%51?43+c%2*2:10));}
