9c875b9c345006822676fccf54c54c48dd6b2a60
Minhas Kamal <minhaskamal024@gmail.com>
2017-02-20 11:52:21 +0600

    Uses most unusual method of calculating PI.

---
+++ pi.c
@@ -0,0 +1,25 @@
+// Source- http://www.ioccc.org/1988/westley.c
+// Intro- Uses most unusual method of calculating PI. It works by calculating the area and diameter of the circle, and then doing a division.
+// Details- http://reasonableapproximation.net/2014/01/18/pi-py.html
+
+#define _ -F<00||--F-OO--;
+int F=00,OO=00;main(){F_OO();printf("%1.3f\n",4.*-F/OO/OO);}F_OO()
+{
+            _-_-_-_
+       _-_-_-_-_-_-_-_-_
+    _-_-_-_-_-_-_-_-_-_-_-_
+  _-_-_-_-_-_-_-_-_-_-_-_-_-_
+ _-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+ _-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+ _-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+ _-_-_-_-_-_-_-_-_-_-_-_-_-_-_
+  _-_-_-_-_-_-_-_-_-_-_-_-_-_
+    _-_-_-_-_-_-_-_-_-_-_-_
+        _-_-_-_-_-_-_-_
+            _-_-_-_
+}
+
