e4ef82769df742aaa2224eed8ad33292747ae75a
Minhas Kamal <minhaskamal024@gmail.com>
2017-02-20 11:58:26 +0600

    Uses the most unusual method of calculating PI.

--- pi.c
+++ pi.c
@@ -1,6 +1,6 @@
 // Source- http://www.ioccc.org/1988/westley.c
-// Intro- Uses most unusual method of calculating PI. It works by calculating the area and diameter of the circle, and then doing a division.
-// Details- http://reasonableapproximation.net/2014/01/18/pi-py.html
+// Intro- Uses the most unusual method of calculating PI. It works by calculating the area and diameter of the circle, and then doing a division.
+// Details- http://www.cplusplus.com/forum/general/13434/
 
 #define _ -F<00||--F-OO--;
 int F=00,OO=00;main(){F_OO();printf("%1.3f\n",4.*-F/OO/OO);}F_OO()
