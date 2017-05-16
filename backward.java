a37751181a398803cceee8e340f6084711a2b1b1
Minhas Kamal <minhaskamal024@gmail.com>
2017-05-16 12:13:29 +0600

    Strange flipped code compiles- "backward!".

--- backward.java
+++ backward.java
@@ -1,7 +1,4 @@
-/*
-This code prints Hello World!
-Can you figure out why ?
-Run it here : http://ideone.com/4heX2G
-Can't figure it out ? There is a StackOverflow question posted by me : http://stackoverflow.com/questions/43943699/why-does-this-code-written-backwards-print-hello-world
-*/
-class M‮{public static void main(String[]a‭){System.out.print(new char[]{'H','e','l','l','o',' ','W','o','r','l','d','!'});}}
+// Intro- Strange flipped code compiles- "backward!".
+// Details- http://stackoverflow.com/questions/43943699/why-does-this-code-written-backwards-print-hello-world
+
+public class backward{public static void main(String[]a‭‮){System.out.print(new char[]{'b','a','c','k','w','a','r','d','!'});}}
