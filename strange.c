87af68fbacf53b5868f66204f08c77ee6b16d581
Minhas Kamal <minhaskamal024@gmail.com>
2017-06-06 09:10:14 +0600

    A strange code with more strange output.

---
+++ strange.c
@@ -0,0 +1,29 @@
+// Developer- Minhas Kamal (https://minhaskamal.github.io)
+// Intro- A strange code with more strange output.
+// Details- https://www.quora.com/What-are-some-of-the-most-strange-programming-language-features/answer/Minhas-Kamal?srid=u2HNX
+
+#include <stdio.h>
+
+int ma\
+in() {                             /// [1]
+
+    int arr[10], i;
+
+    for(i=0; i<10; i++){
+        i[arr] = i;                /// [2]
+    }
+
+    while(i --> 0){                /// [3]
+        printf("%d ", *arr+i);     /// [4]
+    }
+
+    https://www.google.com         /// [5]
+
+    i = 010;
+    printf("\n| %d |", i);         /// [6]
+
+    i = 5i;
+    printf("\n %d", i);            /// [7]
+
+    return 0;
+}
