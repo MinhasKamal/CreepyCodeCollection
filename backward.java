feeb98130c56bd92c495a9bd9c9643fcfbc9ed36
Minhas Kamal <minhaskamal024@gmail.com>
2017-06-06 09:22:34 +0600

    Weird flipped code compiles- "backward!".

--- backward.java
+++ backward.java
@@ -1,4 +1,4 @@
-// Intro- Strange flipped code compiles- "backward!".
+// Intro- Weird flipped code compiles- "backward!".
 // Details- http://stackoverflow.com/questions/43943699/why-does-this-code-written-backwards-print-hello-world
 
 public class backward{public static void main(String[]a‭‮){System.out.print(new char[]{'b','a','c','k','w','a','r','d','!'});}}
