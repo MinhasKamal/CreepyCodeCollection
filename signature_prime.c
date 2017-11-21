1a5dc8a70cb7726df5f447618f57667a2b7f15ab
Minhas Kamal <minhaskamal024@gmail.com>
2017-11-21 17:38:43 +0600

    Keeps printing prime number (theoretically) forever.

--- signature_prime.c
+++ signature_prime.c
@@ -3,4 +3,4 @@
 //      Can be made shorter (53 bytes)- main(m,k){for(;m%k--?:(k=m++);k^1?:printf("%i|",m));}
 // Details- https://www.quora.com/What-is-the-shortest-and-most-effective-code-ever-written/answer/Minhas-Kamal?srid=u2HNX
 
-_=1;main($){for(;_%$?:(_+=$=1);++$<=sqrt(_)?:printf("%i|",$=_));}
+_=1;main($){for(;_%$?:(_+=$=1);++$*$<=_?:printf("%i|",$=_));}
